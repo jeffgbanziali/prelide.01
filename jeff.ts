458326696ba2e8fdaf44cfaa2afbf9aa39f0cf93
Jeff Gbanziali <you@example.com>
2021-10-04 11:10:56 +0200

    Adding jefft.ts

--- jeff.ts
+++ jeff.ts
@@ -1,11 +1,11 @@
-function lowerCase(str = '') {
+function upperCase(str = '') {
     let result = ''
     
     for (let i=0; i < str.length; i++) {
       const code = str.charCodeAt(i)
       
-      if (code >= 65 && code <= 90) {
-        result += String.fromCharCode(code + 32)
+      if (code >= 97 && code <= 122) {
+        result += String.fromCharCode(code -32)
       } else {
         result += str[i] 
       }
@@ -14,5 +14,5 @@ function lowerCase(str = '') {
     return result
   }
   
-  console.log(lowerCase('Chopper'))
-  console.log(lowerCase('TONY TONY'))
+  console.log(upperCase('chopper'))
+  console.log(upperCase('tony tony'))
